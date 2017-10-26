8625f8671381782278953db92b3cb2f192e89b51
Fabian Buentello <fbuentelloapp@gmail.com>
2017-10-25 19:35:11 -0500

    Update html file

--- src/main/MainWindow.ts
+++ src/main/MainWindow.ts
@@ -1,11 +1,14 @@
 import { BrowserWindow } from 'electron'
+import { INDEX_HTML_PATH } from '../utils/constants'
+
 
 let mainWindow: BrowserWindow
 
-export const MainWindow = (URL): BrowserWindow => {
+export const MainWindow = (): BrowserWindow => {
     mainWindow = new BrowserWindow()
 
-    mainWindow.loadURL(URL)
+    mainWindow.loadURL(INDEX_HTML_PATH)
+
     if (process.env.NODE_ENV === 'development') {
         mainWindow.webContents.openDevTools()
     }
--- src/main/main.ts
+++ src/main/main.ts
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV === 'development') {
     require('electron-compile').enableLiveReload()
 }
 
-import { app, ipcMain, BrowserWindow } from 'electron'
+import { app, ipcMain } from 'electron'
 import { MainWindow } from './MainWindow'
 import { StoreFactory, StoreHelpers } from '../store/store'
 import { INITIAL_STATE_FILE_PATH } from '../utils/constants'
@@ -11,17 +11,22 @@ import { autoUpdater } from "electron-updater"
 import log from 'electron-log'
 import { INDEX_HTML_PATH } from '../utils/constants'
 
-let menubarWindow: any
-let mainWindow: BrowserWindow | null
+// let menubarWindow: any
+let mainWindow: any
 
 let store = StoreFactory()
+
+ipcMain.on('GET_VERSION_NUMBER', (event, arg) => {
+    event.sender.send('VERSION_MESSAGE', app.getVersion())
+})
 
 app.on('ready', () => {
     console.log(`VERSION: ${app.getVersion()}`)
     console.log('App is ready')
-    menubarWindow = MenuBar()
-    mainWindow = MainWindow(`${INDEX_HTML_PATH}#v${app.getVersion()}`)
-    autoUpdater.checkForUpdates();
+    // menubarWindow = MenuBar()
+    mainWindow = MainWindow()
+
+    autoUpdater.checkForUpdates()
     mainWindow.on('close', () => {
         console.log('Writing state to file')
         StoreHelpers.writeStateToFile(INITIAL_STATE_FILE_PATH)
@@ -31,7 +36,7 @@ app.on('ready', () => {
 
 app.on('activate', () => {
     if (mainWindow === null) {
-        mainWindow = MainWindow(`${INDEX_HTML_PATH}#v${app.getVersion()}`)
+        mainWindow = MainWindow()
     }
 })
 
@@ -42,34 +47,35 @@ app.on('window-all-closed', function () {
 })
 
 function sendStatusToWindow(text) {
-    log.info(text);
-    (mainWindow as BrowserWindow).webContents.send('message', text);
+    log.info(text)
+    mainWindow.webContents.send('MESSAGE', text)
 }
 
 autoUpdater.on('checking-for-update', () => {
-    sendStatusToWindow('Checking for update...');
+    console.log('AGGGHGHHGG')
+    sendStatusToWindow('Checking for update...')
 })
 autoUpdater.on('update-available', (info) => {
-    sendStatusToWindow('Update available.');
+    sendStatusToWindow('Update available.')
 })
 autoUpdater.on('update-not-available', (info) => {
-    sendStatusToWindow('Update not available.');
+    sendStatusToWindow('Update not available.')
 })
 autoUpdater.on('error', (err) => {
-    sendStatusToWindow('Error in auto-updater.');
+    sendStatusToWindow('Error in auto-updater.')
 })
 autoUpdater.on('download-progress', (progressObj) => {
-    let log_message = "Download speed: " + progressObj.bytesPerSecond;
-    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-    sendStatusToWindow(log_message);
+    let log_message = "Download speed: " + progressObj.bytesPerSecond
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%'
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
+    sendStatusToWindow(log_message)
 })
 autoUpdater.on('update-downloaded', (info) => {
-    sendStatusToWindow('Update downloaded; will install in 5 seconds');
+    sendStatusToWindow('Update downloaded will install in 5 seconds')
     // Wait 5 seconds, then quit and install
     // In your application, you don't need to wait 5 seconds.
-    // You could call autoUpdater.quitAndInstall(); immediately
+    // You could call autoUpdater.quitAndInstall() immediately
     setTimeout(function () {
-        autoUpdater.quitAndInstall();
+        autoUpdater.quitAndInstall()
     }, 5000)
 })
