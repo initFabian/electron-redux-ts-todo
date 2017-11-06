750f3936d47ff5eb0ea9a815cf350087f2e3c2d4
Fabian Buentello <fbuentelloapp@gmail.com>
2017-11-06 06:02:03 -0600

    Auto updater through electron-forge

--- src/main/main.ts
+++ src/main/main.ts
@@ -2,14 +2,18 @@ if (process.env.NODE_ENV === 'development') {
     require('electron-compile').enableLiveReload()
 }
 
-import { app, ipcMain } from 'electron'
+import { app, ipcMain, autoUpdater } from 'electron'
 import { MainWindow } from './MainWindow'
 import { StoreFactory, StoreHelpers } from '../store/store'
 import { INITIAL_STATE_FILE_PATH } from '../utils/constants'
 import MenuBar from './MenuBar'
-import { autoUpdater } from "electron-updater"
-import log from 'electron-log'
 import { INDEX_HTML_PATH } from '../utils/constants'
+import * as os from 'os'
+
+var platform = os.platform() + '_' + os.arch()
+var version = app.getVersion()
+console.log(platform)
+autoUpdater.setFeedURL(`https://electron-todo-updater.herokuapp.com/update/${platform}/${version}`)
 
 // let menubarWindow: any
 let mainWindow: any
@@ -26,7 +30,7 @@ app.on('ready', () => {
     // menubarWindow = MenuBar()
     mainWindow = MainWindow()
 
-    autoUpdater.checkForUpdates()
+    // autoUpdater.checkForUpdates()
     mainWindow.on('close', () => {
         console.log('Writing state to file')
         StoreHelpers.writeStateToFile(INITIAL_STATE_FILE_PATH)
@@ -47,35 +51,34 @@ app.on('window-all-closed', function () {
 })
 
 function sendStatusToWindow(text) {
-    log.info(text)
     mainWindow.webContents.send('MESSAGE', text)
 }
 
-autoUpdater.on('checking-for-update', () => {
-    console.log('AGGGHGHHGG')
-    sendStatusToWindow('Checking for update...')
-})
-autoUpdater.on('update-available', (info) => {
-    sendStatusToWindow('Update available.')
-})
-autoUpdater.on('update-not-available', (info) => {
-    sendStatusToWindow('Update not available.')
-})
-autoUpdater.on('error', (err) => {
-    sendStatusToWindow('Error in auto-updater.')
-})
-autoUpdater.on('download-progress', (progressObj) => {
-    let log_message = "Download speed: " + progressObj.bytesPerSecond
-    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%'
-    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
-    sendStatusToWindow(log_message)
-})
-autoUpdater.on('update-downloaded', (info) => {
-    sendStatusToWindow('Update downloaded will install in 5 seconds')
-    // Wait 5 seconds, then quit and install
-    // In your application, you don't need to wait 5 seconds.
-    // You could call autoUpdater.quitAndInstall() immediately
-    setTimeout(function () {
-        autoUpdater.quitAndInstall()
-    }, 5000)
-})
+// autoUpdater.on('checking-for-update', () => {
+//     console.log('AGGGHGHHGG')
+//     sendStatusToWindow('Checking for update...')
+// })
+// autoUpdater.on('update-available', (info) => {
+//     sendStatusToWindow('Update available.')
+// })
+// autoUpdater.on('update-not-available', (info) => {
+//     sendStatusToWindow('Update not available.')
+// })
+// autoUpdater.on('error', (err) => {
+//     sendStatusToWindow('Error in auto-updater.')
+// })
+// autoUpdater.on('download-progress', (progressObj) => {
+//     let log_message = "Download speed: " + progressObj.bytesPerSecond
+//     log_message = log_message + ' - Downloaded ' + progressObj.percent + '%'
+//     log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
+//     sendStatusToWindow(log_message)
+// })
+// autoUpdater.on('update-downloaded', (info) => {
+//     sendStatusToWindow('Update downloaded will install in 5 seconds')
+//     // Wait 5 seconds, then quit and install
+//     // In your application, you don't need to wait 5 seconds.
+//     // You could call autoUpdater.quitAndInstall() immediately
+//     setTimeout(function () {
+//         autoUpdater.quitAndInstall()
+//     }, 5000)
+// })
